44c559143b5c09a412aac1d72ac35bf20e35b410
Anita Kis <juhaszanita7@gmail.com>
2018-08-28 16:01:37 -0700

    displayed recipes in a list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'recipes';
+export class AppComponent implements OnInit {
+  recipes = {};
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(){
+    this.http.get('assets/recipes.json').subscribe(res => {
+    this.recipes = res;
+  });
+}
 }
