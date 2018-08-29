86f33bd93af18c4f220aefa39d79b347dd934f6f
Anita Kis <juhaszanita7@gmail.com>
2018-08-28 18:18:48 -0700

    updated the functionality of the recipe selection

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,8 @@ import { HttpClient } from '@angular/common/http';
 export class AppComponent implements OnInit {
   recipes = {};
   selectedRecipes = 0;
+  selectedIngredients = [];
+  ingredientsToShow = [];
 
   constructor(private http: HttpClient) { }
 
@@ -20,5 +22,28 @@ export class AppComponent implements OnInit {
 
   selectRecipe = (event) => {
     event.currentTarget.checked ? this.selectedRecipes++ : this.selectedRecipes--;
+
+    this.updateIngredients(event.currentTarget.checked, event.currentTarget.value)
+  }
+
+  updateIngredients = (checked, index) => {
+      if (checked) {
+        //this.selectedIngredients.push(...this.recipes[index].ingredients);
+        this.selectedIngredients.push(this.recipes[index].ingredients);
+        //console.log(this.selectedIngredients);
+      } else {
+        //console.log(this.selectedIngredients.includes(this.recipes[index].ingredients));
+        this.selectedIngredients.splice(this.selectedIngredients.indexOf(this.recipes[index].ingredients), 1);
+        //console.log('after removal: ', this.selectedIngredients);
+      }
+      
+      this.ingredientsToShow = [].concat(...this.selectedIngredients);
+      //console.log('to show: ', this.ingredientsToShow);
+
+      this.ingredientsToShow.sort();
+      this.ingredientsToShow = this.ingredientsToShow.filter((val, ind, self) => {
+        return self.indexOf(val) === ind;
+      });
+      //console.log('to show after filter: ', this.ingredientsToShow);
   }
 }
