7713e43553b2c02c41e40e6e7a9f6c3746683986
Anita Kis <juhaszanita7@gmail.com>
2018-08-29 14:19:11 -0700

    updated the view

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
   recipes = {};
@@ -22,15 +22,14 @@ export class AppComponent implements OnInit {
 
   selectRecipe = (event) => {
     event.currentTarget.checked ? this.selectedRecipes++ : this.selectedRecipes--;
+    event.currentTarget.parentNode.classList.toggle('selected');
 
-    this.updateIngredients(event.currentTarget.checked, event.currentTarget.value)
+    this.updateIngredients(event.currentTarget.checked, event.currentTarget.value);
   }
 
   updateIngredients = (checked, index) => {
-    checked ?
-      this.selectedIngredients.push(this.recipes[index].ingredients)
-    :
-      this.selectedIngredients.splice(this.selectedIngredients.indexOf(this.recipes[index].ingredients), 1);
+    checked ? this.selectedIngredients.push(this.recipes[index].ingredients)
+            : this.selectedIngredients.splice(this.selectedIngredients.indexOf(this.recipes[index].ingredients), 1);
 
     this.ingredientsToShow = [].concat(...this.selectedIngredients);
     this.ingredientsToShow.sort();
